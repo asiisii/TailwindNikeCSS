8a92fe701b6fb8800aecbc254e9691c460c3c585
asiisii <ashishmalla45@gmail.com>
2023-08-26 19:03:22 -0600

    replace "" with ''

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
-import { hamburger } from "../assets/icons";
-import { headerLogo } from "../assets/images";
-import { navLinks } from "../constants";
+import { hamburger } from '../assets/icons';
+import { headerLogo } from '../assets/images';
+import { navLinks } from '../constants';
 
 const Nav = () => {
   return (
